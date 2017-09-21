e1de08ac3232eae4ffa639a203fda69824c22885
wagenhuber <wagenhuber@gmx.net>
2017-09-21 10:18:00 +0200

    Eventhandling with annoymous inner class

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,6 +1,9 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenster extends JFrame {
 
@@ -17,12 +20,34 @@ public class Fenster extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.initComonents();
-
+this.initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
 
     }
 
+    private void initEvents() {
+        jbtnRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.RED);
+            }
+        });
+
+        jbtnGelb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.YELLOW);
+            }
+        });
+
+        jbtnBLau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.BLUE);
+            }
+        });
+    }
 
 
     private void initComonents() {
