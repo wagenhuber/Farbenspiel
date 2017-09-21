205a3667184ace3fe3205ce4723463cdf84c5a28
wagenhuber <wagenhuber@gmx.net>
2017-09-21 09:01:48 +0200

    Events implemented

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,8 +1,11 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Fenster extends JFrame {
+public class Fenster extends JFrame implements ActionListener {
 
     private JPanel jPanel;
     private JButton jbtnRot;
@@ -15,16 +18,22 @@ public class Fenster extends JFrame {
 
         super("Farbenspiel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         this.initComonents();
-        
+        this.initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
 
     }
 
+    private void initEvents() {
+        jbtnRot.addActionListener(this);
+        jbtnBLau.addActionListener(this);
+        jbtnGelb.addActionListener(this);
+    }
+
     private void initComonents() {
-    jPanel = new JPanel();
+        jPanel = new JPanel();
         jbtnRot = new JButton("Rot");
         jbtnGelb = new JButton("Gelb");
         jbtnBLau = new JButton("Blau");
@@ -33,7 +42,32 @@ public class Fenster extends JFrame {
         jPanel.add(jbtnBLau);
 
         this.add(jPanel);
+
+
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //String command = e.getActionCommand();
+        //System.out.println(command + " wurde geklickt!");
+
+        switch (e.getActionCommand()) {
+            case "Rot":
+                jPanel.setBackground(Color.RED);
+                System.out.println("Rot");
+                break;
+            case "Gelb":
+                jPanel.setBackground(Color.YELLOW);
+                System.out.println("Gelb");
+                break;
+            case "Blau":
+                jPanel.setBackground(Color.BLUE);
+                System.out.println("Blau");
+                break;
+
+        }
+
+
+    }
 }
