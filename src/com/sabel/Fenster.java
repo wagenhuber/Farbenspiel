b8d965aa84b69901c99815c35322c8e2e8016a39
wagenhuber <wagenhuber@gmx.net>
2017-09-21 08:25:43 +0200

    components created

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -15,14 +15,25 @@ public class Fenster extends JFrame {
 
         super("Farbenspiel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.initComonents();
+        
         this.setSize(300, 300);
         this.setVisible(true);
 
     }
 
+    private void initComonents() {
+    jPanel = new JPanel();
+        jbtnRot = new JButton("Rot");
+        jbtnGelb = new JButton("Gelb");
+        jbtnBLau = new JButton("Blau");
+        jPanel.add(jbtnRot);
+        jPanel.add(jbtnGelb);
+        jPanel.add(jbtnBLau);
 
-
-
+        this.add(jPanel);
+    }
 
 
 }
